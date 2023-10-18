cfd7d1c0abb70c210d4cb4b6713434b64ba409de
JuanluisSoto <juanlulusm@gmail.com>
2023-10-18 22:47:35 +0200

    Ver_1.1

--- Entrega1/__Scripts.js
+++ Entrega1/__Scripts.js
@@ -1,5 +1,18 @@
 function Play(){
     alert("Boton jugar pulsado")
+    let mano=document.getElementById("mano").value;
+    let rand=Math.random()*100;
+    let num=parseInt(rand%3);
+    alert(mano+" "+num);
+
+    switch(num){
+        case mano==arrMano[num]:
+            alert(empate);
+        break;
+        case mano!=arrMano[num]:
+            
+        break;
+    }
 };
 
 function Borrar(){
